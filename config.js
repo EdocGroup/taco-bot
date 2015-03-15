e5dce398dc4e027aba81ef811d97c4f2392ac3ec
Max Verigin <leetkr3w@gmail.com>
2015-03-14 18:47:28 -0700

    add default config and data files

---
+++ config.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var config = {
+    jiraUrl: 'https://COMPANY_HERE.atlassian.net/browse/',
+    slackToken: 'SLACK_TOKEN_HERE',
+    githubToken: 'GITHUB_TOKEN_HERE'
+};
+
+module.exports = config;
