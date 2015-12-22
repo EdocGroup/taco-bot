dc1e87c5164245bff9bd1693535bc50c51f6c8f7
gundal <leeam.dagan@edocgroup.com>
2015-12-22 12:59:35 -0800

    added ability to wipe db by ading .YES onto revision

--- actions/updateServer.js
+++ actions/updateServer.js
@@ -3,7 +3,7 @@
 var updateServerAction = {
     command: '!Update-Server:',
     helpDisplayCommand: '!Update-Server:<server>:<fullrevision>',
-    description: 'Utilizes install-releasecandidate.ps1 to update an internal server with a revision of RC',
+    description: 'Utilizes install-releasecandidate.ps1 to update an internal server with a revision of RC. If you want to wipe the DB add .YES to the revision',
     perform: function (options) {
     	
                    var invoke = options.message.text.split(updateServerAction.command)[1].split(" ")[0].trim(); 
@@ -33,6 +33,7 @@ var updateServerAction = {
  var minor = revision.split('.')[1];
  var build = revision.split('.')[2];
  var rev = revision.split('.')[3];
+ var wipe = revision.split('.')[4];
 
   var output = [
         "Server: " + server,
@@ -44,8 +45,8 @@ var updateServerAction = {
 
     ];	
 	
-var result = ("powershell.exe -ExecutionPolicy unrestricted -Command \" & 'tools\\Install-ReleaseCandidate.ps1' -Server '" + server + "' -Revision '" + rev + "' -Build '" + build + "' -Major '" + major + "' -Minor '" + minor + "'\" " );	
-var child = require('child_process').exec("powershell.exe -ExecutionPolicy unrestricted -Command \" & 'tools\\Install-ReleaseCandidate.ps1' -Server '" + server + "' -Revision '" + rev + "' -Build '" + build + "' -Major '" + major + "' -Minor '" + minor + "'\" " );
+var result = ("powershell.exe -ExecutionPolicy unrestricted -Command \" & 'tools\\Install-ReleaseCandidate.ps1' -Server '" + server + "' -Major '" + major + "' -Minor '" + minor +  "' -Build '" + build + "' -Revision '" + rev + "' -Wipe '" + wipe + "'\" " );
+var child = require('child_process').exec("powershell.exe -ExecutionPolicy unrestricted -Command \" & 'tools\\Install-ReleaseCandidate.ps1' -Server '" + server + "' -Revision '" + rev + "' -Build '" + build + "' -Major '" + major + "' -Minor '" + minor + "' -Wipe '" + wipe + "'\" " );
 child.stdout.pipe(process.stdout);
 
 console.log(output);
