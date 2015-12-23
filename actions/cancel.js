591a2add78e445cb4e04dc5d49ee4085cbec52d0
gundal <leeam.dagan@edocgroup.com>
2015-12-22 16:18:45 -0800

    remove cancel from help and the output it produces.

--- actions/cancel.js
+++ actions/cancel.js
@@ -2,15 +2,14 @@
 
 var cancelUpdateServerAction = {
     command: '!Cancel',
-    helpDisplayCommand: '!Cancel // this must be used immediately after !Update-Server',
     description: 'Stops install-releasecandidate.ps1 from continuing its deploy.',
     perform: function (options) {
     	
-                var output = [   "Killing powershell.exe on ED-BLDSEL07" ];										
+                var output = [  "Killing powershell.exe on host machine." ];										
 				var child = require('child_process').exec("cmd.exe /c taskkill.exe /IM powershell.exe /F");
 				child.stdout.pipe(process.stdout);
 
-       return '```' + output + '```';
+       //return '```' + output + '```';
 
 	}
 };
