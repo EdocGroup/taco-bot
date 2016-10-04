ed666081c7ba27598d6cf7834704ef011251c4a8
Tyler Weeres <tweeres@edoc.net>
2016-10-04 10:36:12 -0700

    Updated to work with whitespace; Added a channel whitelist

--- actions/customer.js
+++ actions/customer.js
@@ -1,28 +1,32 @@
 "use strict";
 
 const customerDataFile = './customer/customers.json';
+const customerChannels = require('../config.json').customerChannels;
 
 const customerAction = {
     command: /.*/g,
     helpDisplayCommand: '<customer>',
     description: 'Show customer information',
     perform(options) {
-        delete require.cache[require.resolve(customerDataFile)]
-        const message = options.message.text;
-        const customers = require(customerDataFile);
-        const slugs = Object.keys(customers);
+        if(customerChannels.filter(c => c == options.channel.name).length > 0){
+            delete require.cache[require.resolve(customerDataFile)]
+            const message = options.message.text;
+            const customers = require(customerDataFile);
+            const slugs = Object.keys(customers);
 
-        let match = null;
-        slugs.some(slug => {
-            if(message.indexOf(slug) != -1){
-                match = slug;
-                return true;
+            let match = null;
+            slugs.some(slug => {
+                const regex = new RegExp(`.*(^|\\W)${slug}($|\\W).*`, 'i');
+                if(message.match(regex)){
+                    match = slug;
+                    return true;
+                }
+                return false;
+            });
+            if(match){
+                const customer = customers[match];
+                return `\`\`\`${customer}\`\`\``;
             }
-            return false;
-        });
-        if(match){
-            const customer = customers[match];
-            return `\`\`\`${customer}\`\`\``;
         }
     }
 };
